6714fda651d9fab7677ce07ed928aea625074e0d
HISHORE N <55741570+Hishore20@users.noreply.github.com>
2020-12-29 20:09:05 +0530

    Create Ex2.py

---
+++ Ex2.py
@@ -0,0 +1,16 @@
+def find_product(num1,num2,num3):
+    product=0
+    if (num1 != 7 and num2 != 7 and num3 != 7):
+        product = num1 * num2 * num3
+    elif (num1 == 7):
+        product = num2 * num3
+    elif(num2 == 7):
+        product = num3
+    elif(num3 == 7):
+        product = -1
+    return product
+a=int(input())
+b=int(input())
+c=int(input())
+product=find_product(a,b,c)
+print(product)
